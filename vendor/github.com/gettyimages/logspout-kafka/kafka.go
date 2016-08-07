011177bb600f7e20e7ae681335499f1dced1910b
saiwl <saiwl@zhihu.com>
2016-08-07 19:28:49 +0800

    use vendor;reused the kafka producer to avoid too many tcp links

---
+++ vendor/github.com/gettyimages/logspout-kafka/kafka.go
@@ -0,0 +1,175 @@
+package kafka
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/gliderlabs/logspout/router"
+	"gopkg.in/Shopify/sarama.v1"
+)
+
+func init() {
+	router.AdapterFactories.Register(NewKafkaAdapter, "kafka")
+}
+
+var KafkaProducerMap = make(map[string]sarama.AsyncProducer)
+
+type KafkaAdapter struct {
+	route    *router.Route
+	brokers  []string
+	topic    string
+	producer sarama.AsyncProducer
+	tmpl     *template.Template
+}
+
+func NewKafkaAdapter(route *router.Route) (router.LogAdapter, error) {
+	brokers := readBrokers(route.Address)
+	if len(brokers) == 0 {
+		return nil, errorf("The Kafka broker host:port is missing. Did you specify it as a route address?")
+	}
+
+	topic := readTopic(route.Address, route.Options)
+	if topic == "" {
+		return nil, errorf("The Kafka topic is missing. Did you specify it as a route option?")
+	}
+
+	var err error
+	var tmpl *template.Template
+	if text := os.Getenv("KAFKA_TEMPLATE"); text != "" {
+		tmpl, err = template.New("kafka").Parse(text)
+		if err != nil {
+			return nil, errorf("Couldn't parse Kafka message template. %v", err)
+		}
+	}
+
+	if os.Getenv("DEBUG") != "" {
+		log.Printf("Starting Kafka producer for address: %s, topic: %s.\n", brokers, topic)
+	}
+
+	var retries int
+	retries, err = strconv.Atoi(os.Getenv("KAFKA_CONNECT_RETRIES"))
+	if err != nil {
+		retries = 3
+	}
+	var producer sarama.AsyncProducer
+	if producer, ok := KafkaProducerMap[strings.Join(brokers, ",")]; ok {
+		log.Println("producer already have, return directly")
+		return &KafkaAdapter{
+			route:    route,
+			brokers:  brokers,
+			topic:    topic,
+			producer: producer,
+			tmpl:     tmpl,
+		}, nil
+	}
+	for i := 0; i < retries; i++ {
+		producer, err = sarama.NewAsyncProducer(brokers, newConfig())
+		if err != nil {
+			if os.Getenv("DEBUG") != "" {
+				log.Println("Couldn't create Kafka producer. Retrying...", err)
+			}
+			if i == retries-1 {
+				return nil, errorf("Couldn't create Kafka producer. %v", err)
+			}
+		} else {
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	KafkaProducerMap[strings.Join(brokers,",")] = producer
+
+	return &KafkaAdapter{
+		route:    route,
+		brokers:  brokers,
+		topic:    topic,
+		producer: producer,
+		tmpl:     tmpl,
+	}, nil
+}
+
+func (a *KafkaAdapter) Stream(logstream chan *router.Message) {
+	defer a.producer.Close()
+	for rm := range logstream {
+		message, err := a.formatMessage(rm)
+		if err != nil {
+			log.Println("kafka:", err)
+			a.route.Close()
+			break
+		}
+
+		a.producer.Input() <- message
+	}
+}
+
+func newConfig() *sarama.Config {
+	config := sarama.NewConfig()
+	config.ClientID = "logspout"
+	config.Producer.Return.Errors = false
+	config.Producer.Return.Successes = false
+	config.Producer.Flush.Frequency = 1 * time.Second
+	config.Producer.RequiredAcks = sarama.WaitForLocal
+
+	if opt := os.Getenv("KAFKA_COMPRESSION_CODEC"); opt != "" {
+		switch opt {
+		case "gzip":
+			config.Producer.Compression = sarama.CompressionGZIP
+		case "snappy":
+			config.Producer.Compression = sarama.CompressionSnappy
+		}
+	}
+
+	return config
+}
+
+func (a *KafkaAdapter) formatMessage(message *router.Message) (*sarama.ProducerMessage, error) {
+	var encoder sarama.Encoder
+	if a.tmpl != nil {
+		var w bytes.Buffer
+		if err := a.tmpl.Execute(&w, message); err != nil {
+			return nil, err
+		}
+		encoder = sarama.ByteEncoder(w.Bytes())
+	} else {
+		encoder = sarama.StringEncoder(message.Data)
+	}
+
+	return &sarama.ProducerMessage{
+		Topic: a.topic,
+		Value: encoder,
+	}, nil
+}
+
+func readBrokers(address string) []string {
+	if strings.Contains(address, "/") {
+		slash := strings.Index(address, "/")
+		address = address[:slash]
+	}
+
+	return strings.Split(address, ",")
+}
+
+func readTopic(address string, options map[string]string) string {
+	var topic string
+	if !strings.Contains(address, "/") {
+		topic = options["topic"]
+	} else {
+		slash := strings.Index(address, "/")
+		topic = address[slash+1:]
+	}
+
+	return topic
+}
+
+func errorf(format string, a ...interface{}) (err error) {
+	err = fmt.Errorf(format, a...)
+	if os.Getenv("DEBUG") != "" {
+		fmt.Println(err.Error())
+	}
+	return
+}
